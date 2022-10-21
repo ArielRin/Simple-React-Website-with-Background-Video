ae071344cd41cfcfa3dd79a01060c6819757c4af
Cheyne Hayworth <104659633+ArielRin@users.noreply.github.com>
2022-10-22 00:26:07 +1100

    nav bar linking and theme

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,16 +9,17 @@ const Hero = () => {
         <nav>
             <ul>
                 <li>
-                  <a href="https://swap.sirenstreasure.tk" target="_blank" rel="noopener noreferrer">Exchange</a>
+                  <ul href="https://swap.sirenstreasure.tk" target="_blank" rel="noopener noreferrer">Exchange</ul>
                 </li>
                 <li>
-                  <a href="https://docs.sirenstreasure.tk" color='#fff' target="_blank" rel="noopener noreferrer">Documentation</a>
+                  <ul href="https://docs.sirenstreasure.tk" color='#fff' target="_blank" rel="noopener noreferrer">Documentation</ul>
                 </li>
                 <li>
-                  <a href="https://nft.sirenstreasure.tk" target="_blank" rel="noopener noreferrer">Mermaid Friends NFT's</a>
+                  <ul href="https://nft.sirenstreasure.tk" target="_blank" rel="noopener noreferrer">Mermaid Friends NFT's</ul>
                 </li>
             </ul>
         </nav>
+        <nav src="/images/logo.gif" alt="LP Treasures" width={850} height={230} />
         <div className="overlay"></div>
         <video src={BgVideo} autoPlay loop muted />
 
